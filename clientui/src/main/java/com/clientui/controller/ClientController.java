f5862e81a5b93b3f8183284d416a3d7a96c45471
lkhadir <lkhadir.mostapha@gmail.com>
2021-11-30 17:43:04 +0100

    handle exception of microservice-produits

--- clientui/src/main/java/com/clientui/controller/ClientController.java
+++ clientui/src/main/java/com/clientui/controller/ClientController.java
@@ -5,6 +5,7 @@ import com.clientui.proxies.MicroserviceProduitsProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -21,5 +22,11 @@ public class ClientController {
         List<ProductBean> produits = ProduitsProxy.listeDesProduits();
         model.addAttribute("produits",produits);
         return "Accueil";
+    }
+    @RequestMapping("/details-produit/{id}")
+    public String fichProduit(@PathVariable int id, Model model){
+        ProductBean produit = ProduitsProxy.recupererUnProduit(id);
+        model.addAttribute("produit",produit);
+        return "FicheProduit";
     }
 }
