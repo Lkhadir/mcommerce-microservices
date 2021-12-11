a81154b274522b37bf4fe25036801b21b25638f3
lkhadir <lkhadir.mostapha@gmail.com>
2021-12-11 16:21:33 +0100

    add filters to api gateway

---
+++ api-gateway/src/main/java/com/mcommerce/apigateway/filters/LogFilter.java
@@ -0,0 +1,48 @@
+package com.mcommerce.apigateway.filters;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+import java.util.Collections;
+import java.util.Set;
+
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.*;
+
+
+@Component
+public class LogFilter implements GlobalFilter, Ordered {
+
+    Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        Set<URI> uris = exchange.getAttributeOrDefault(GATEWAY_ORIGINAL_REQUEST_URL_ATTR, Collections.emptySet());
+        String originalUri = (uris.isEmpty()) ? "Unknown" : uris.iterator().next().toString();
+        log.info("** Requête interceptée ! L'URL est "  + originalUri);
+
+
+        return chain.filter(exchange);
+
+    }
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+    @Bean
+    public GlobalFilter postGlobalFilter() {
+        return (exchange, chain) -> {
+            return chain.filter(exchange)
+                    .then(Mono.fromRunnable(() -> {
+                        log.info("Last Post Global Filter");
+                    }));
+        };
+    }
+}
